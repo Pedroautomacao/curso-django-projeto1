75721ae0f53897ac7209992f47db13424b0a38b7
Pedro Lucas Andrade <pedroautomacao@hotmail.com>
2022-06-30 10:26:39 -0300

    url

--- recipes/urls.py
+++ recipes/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.urls import path
 
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 urlpatterns = [
     path('', home),  # Home
-    path('sobre/', sobre),  # /sobre/
-    path('contato/', contato),  # /contato/
 ]
