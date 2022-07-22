1cfba7e33a281e52a2085a40eb71e666c2285e93
Pedro Lucas Andrade <pedroautomacao@hotmail.com>
2022-07-22 09:45:06 -0300

    html

--- recipes/views.py
+++ recipes/views.py
@@ -7,6 +7,6 @@ def home(request):
     })
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/recipe-views.html', context={
         'name': 'Pedro Lucas',
     })
